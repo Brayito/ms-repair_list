487264f53668f78759e28a7799be72b16b1dc8e3
Brayito <bryan.sepulveda@usach.cl>
2024-06-13 16:44:58 -0400

    add files

---
+++ ms-repair_list/ms-repair_list/src/main/java/autofix/ms_repair_list/MsRepairListApplication.java
@@ -0,0 +1,13 @@
+package autofix.ms_repair_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRepairListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRepairListApplication.class, args);
+	}
+
+}
---
+++ ms-repair_list/ms-repair_list/src/test/java/autofix/ms_repair_list/MsRepairListApplicationTests.java
@@ -0,0 +1,13 @@
+package autofix.ms_repair_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRepairListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
